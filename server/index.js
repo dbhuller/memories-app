4a06e6e620ab550dc2072b3df03fe285df19aa93
dbhuller <dbhuller@mail.sfsu.edu>
2021-01-11 18:36:29 -0800

    set up client for development later, connect MongoDB Atlas to server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,36 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
+// Need latest version of node (14 or greater to use)
+// import express from "express";
+// import bodyParser from "body-parser";
+// import mongoose from "mongoose";
+// import cors from "cors";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// Configure DB
+const CONNECTION_URL =
+  "mongodb+srv://dbhuller:deep1234@cluster0.kndho.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server running on PORT: ${PORT}`);
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+mongoose.set("useFindAndModify", false);
