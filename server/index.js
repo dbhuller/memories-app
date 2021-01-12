ee2f86fd4c58008f72a19ef32048f1e3ca059b45
dbhuller <dbhuller@mail.sfsu.edu>
2021-01-11 23:48:26 -0800

    added controllers folder to hold route logic

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 // Need latest version of node (14 or greater to use)
-// import express from "express";
-// import bodyParser from "body-parser";
-// import mongoose from "mongoose";
-// import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require("cors");
+import postRoutes from "./routes/posts.js";
+
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 // Configure DB
 const CONNECTION_URL =
