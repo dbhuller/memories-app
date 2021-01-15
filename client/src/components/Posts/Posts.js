0ba111a591c4a31351ff52d64c96ea99e37baba5
dbhuller <dbhuller@mail.sfsu.edu>
2021-01-14 23:48:29 -0800

    added functionality to edit posts, not working correctly need to debug, form fields populate, but after submit and refresh, no changes

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,18 +2,28 @@ import React from "react";
 import Post from "./Post/Post";
 import useStyles from "./styles";
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
 
   console.log(posts);
-  return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.mainContainer}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid item key={post._id} xs={12} sm={6}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
